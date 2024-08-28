d43a4c939c8cadf14199d7f320caee90f8adf636
Atul Baluni <atul.baluni@shoplc.com>
2024-08-28 10:29:38 +0530

    added next & previous asset

--- src/helper/mapper/mapChannelEpg.js
+++ src/helper/mapper/mapChannelEpg.js
@@ -1,13 +1,15 @@
 export function mapChannelEpg(response) {
-    return response.map(channel => ({
-      id: channel.id,
-      title: channel.title,
-      playUrl: channel.playUrl,
-      description: channel.description,
-      image: channel.images,
-      baseSourceLocation: channel.baseSourceLocation,
-      categories: channel.categories.map(category => category.name),
-      schedules: channel.schedules,
-      layout: "horizontal"
-    }));
+  return response.map((channel, index) => ({
+    id: channel.id,
+    title: channel.title,
+    playUrl: channel.playUrl,
+    description: channel.description,
+    image: channel.images,
+    baseSourceLocation: channel.baseSourceLocation,
+    categories: channel.categories.map(category => category.name),
+    schedules: channel.schedules,
+    layout: "horizontal",
+    previousChannel: index === 0 ? null : response[index - 1], // Correctly fetch the previous channel
+    nextChannel: index === response.length - 1 ? null : response[index + 1] // Fetch the next channel, if it exists
+  }));
   }
