978933b05b1ed27ec2ae7f65c6f5f8272f3bf8d2
Atul Baluni <atul.baluni@shoplc.com>
2024-09-05 18:43:02 +0530

    player control & font family

--- src/components/Player/PlayerControls.js
+++ src/components/Player/PlayerControls.js
@@ -8,9 +8,14 @@ import discover from "../../assets/images/discover.png";
 import ControlToggle from "./ControlToggle";
 import VerticalList from "../../helper/VerticalList";
 import { mapChannel } from "../../helper/mapper/mapChannelEpg";
-import downArrow from "../../assets/images/downArrow.png"
+import downArrow from "../../assets/images/downArrow.png";
 import ProgramDetail from "../programDetail.js/ProgramDetail";
-const PlayerControls = ({ selectedAsset, setSelectedAsset,bufferedEnd,  onSeek  }) => {
+const PlayerControls = ({
+  selectedAsset,
+  setSelectedAsset,
+  bufferedEnd,
+  onSeek,
+}) => {
   const { isActive, setIsActive } = useContext(VideoContext);
   const { currentTime } = useContext(VideoContext);
   const [previousChannel, setPreviousChannel] = useState(null);
@@ -44,101 +49,123 @@ const PlayerControls = ({ selectedAsset, setSelectedAsset,bufferedEnd,  onSeek  
   useEffect(() => {
     const percentage = (currentTime / bufferedEnd) * 100;
     setValue(percentage);
-}, [currentTime, bufferedEnd]);
+  }, [currentTime, bufferedEnd]);
 
-const handleSeekChange = (e) => {
+  const handleSeekChange = (e) => {
     const newTime = (e.target.value / 100) * bufferedEnd;
     setValue(e.target.value);
     onSeek(newTime);
-};
+  };
 
   return (
     <>
-    <VerticalList
-      onFocus={(index) => onFocus(index, globals.COMPONENT_NAME.Player_Detail)}
-      onBlur={(index) => handleSetActive(true, index)}
-      className="w-full justify-center gap-3 items-center text-2xl flex"
-      retainLastFocus={true}
-      id={globals.COMPONENT_NAME.Player_Detail}
-      style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
-    >
-      <ProgramDetail />
-    </VerticalList>
-    <div
-      style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
-      className="flex justify-center absolute bottom-2 right-40  w-[80%] h-[16%] m-auto z-80"
-    >
-
-
-      <VerticalList>
-        <HorizontalList
-          onFocus={(index) => onFocus(index, globals.COMPONENT_NAME.Player_Control)}
-          onBlur={(index) => handleSetActive(true, index)}
-          className="w-full justify-center gap-3 items-center text-2xl flex"
-          retainLastFocus={true}
-          id={globals.COMPONENT_NAME.Player_Control}
-        >
-          <div id="seekbar">
-              <ToggleItem className=""> 
-              <input  type="range"  min="0"  max="100"  value={value} onChange={handleSeekChange} className="seek-bar"/>
-              </ToggleItem>
-          </div>
-          
-        </HorizontalList>
-
-        <HorizontalList
-          onFocus={(index) => onFocus(index, globals.COMPONENT_NAME.Player_Control)}
-          onBlur={(index) => handleSetActive(true, index)}
-          className="w-full justify-center gap-3 items-center text-2xl flex"
-          retainLastFocus={true}
-          id={globals.COMPONENT_NAME.Player_Control}
-        >
-          <div className="player-next-prev">
-            <ToggleItem
-              allowedDirection={"right"}
-              parentId={globals.COMPONENT_NAME.Player_Control}
-              disabled={previousChannel ? false : true}
-              className={
-                previousChannel
-                  ? "absolute bottom-10 left-[125px]"
-                  : "disabled-button"
-              }
-              onEnter={() => setSelectedAsset(previousChannel)}
-            >
-              <div className="channel-detail">pre</div>
-              <div className="channel-image">
-
-              <img src={previousChannel?previousChannel.baseSourceLocation+previousChannel.image.logo.tv:""}  alt="runnTV_downArrow"></img>
-              </div>
-            </ToggleItem>
-                   <>
-                   <img id="downArrow" className="absolute bottom-10" src={downArrow} alt="runnTV_downArrow"></img>
-                   </>
-            
-            <ToggleItem
-              allowedDirection={"left"}
-              parentId={globals.COMPONENT_NAME.Player_Control}
-              disabled={nextChannel ? false : true}
-              className={
-                nextChannel
-                  ? "absolute bottom-10 right-[110px]"
-                  : "disabled-button"
-              }
-              onEnter={() => setSelectedAsset(nextChannel)}
-            >
-             
-              <div className="channel-image">
-
-              <img src={nextChannel?nextChannel.baseSourceLocation+nextChannel.image.logo.tv:""}  alt="runnTV_downArrow"></img>
-              </div>
-              <div className="channel-detail">next</div>
-            </ToggleItem>
-          </div>
-        </HorizontalList>
-
-        
+      <VerticalList
+        onFocus={(index) =>
+          onFocus(index, globals.COMPONENT_NAME.Player_Detail)
+        }
+        onBlur={(index) => handleSetActive(true, index)}
+        className="w-full justify-center gap-3 items-center text-2xl flex"
+        retainLastFocus={true}
+        id={globals.COMPONENT_NAME.Player_Detail}
+        style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
+      >
+        <ProgramDetail />
       </VerticalList>
-    </div>
+      <div
+        style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
+        className="flex justify-center absolute bottom-2 right-40  w-[80%] h-[16%] m-auto z-80"
+      >
+        <VerticalList>
+          <HorizontalList
+            onFocus={(index) =>
+              onFocus(index, globals.COMPONENT_NAME.Player_Control)
+            }
+            onBlur={(index) => handleSetActive(true, index)}
+            className="w-full justify-center gap-3 items-center text-2xl flex"
+            retainLastFocus={true}
+            id={globals.COMPONENT_NAME.Player_Control}
+          >
+            <div id="seekbar">
+              <ToggleItem className="">
+                {/* <input  type="range"  min="0"  max="100"  value={value} onChange={handleSeekChange} className="seek-bar"/> */}
+                <div className="seek-bar">
+                  <div className="filled" style={{ width: value+"%" }}></div>
+                </div>
+              </ToggleItem>
+            </div>
+          </HorizontalList>
+
+          <HorizontalList
+            onFocus={(index) =>
+              onFocus(index, globals.COMPONENT_NAME.Player_Control)
+            }
+            onBlur={(index) => handleSetActive(true, index)}
+            className="w-full justify-center gap-3 items-center text-2xl flex"
+            retainLastFocus={true}
+            id={globals.COMPONENT_NAME.Player_Control}
+          >
+            <div className="player-next-prev">
+              <ToggleItem
+                allowedDirection={"right"}
+                parentId={globals.COMPONENT_NAME.Player_Control}
+                disabled={previousChannel ? false : true}
+                className={
+                  previousChannel
+                    ? "absolute bottom-10 left-[125px]"
+                    : "disabled-button"
+                }
+                onEnter={() => setSelectedAsset(previousChannel)}
+              >
+                <div className="channel-detail">pre</div>
+                <div className="channel-image">
+                  <img
+                    src={
+                      previousChannel
+                        ? previousChannel.baseSourceLocation +
+                          previousChannel.image.logo.tv
+                        : ""
+                    }
+                    alt="runnTV_downArrow"
+                  ></img>
+                </div>
+              </ToggleItem>
+              <>
+                <img
+                  id="downArrow"
+                  className="absolute bottom-10"
+                  src={downArrow}
+                  alt="runnTV_downArrow"
+                ></img>
+              </>
+
+              <ToggleItem
+                allowedDirection={"left"}
+                parentId={globals.COMPONENT_NAME.Player_Control}
+                disabled={nextChannel ? false : true}
+                className={
+                  nextChannel
+                    ? "absolute bottom-10 right-[110px]"
+                    : "disabled-button"
+                }
+                onEnter={() => setSelectedAsset(nextChannel)}
+              >
+                <div className="channel-image">
+                  <img
+                    src={
+                      nextChannel
+                        ? nextChannel.baseSourceLocation +
+                          nextChannel.image.logo.tv
+                        : ""
+                    }
+                    alt="runnTV_downArrow"
+                  ></img>
+                </div>
+                <div className="channel-detail">next</div>
+              </ToggleItem>
+            </div>
+          </HorizontalList>
+        </VerticalList>
+      </div>
     </>
   );
 };
