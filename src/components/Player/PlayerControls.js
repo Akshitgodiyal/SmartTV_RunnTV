cce88301807191451bba5550cc1b4fc6a291b1ca
Atul Baluni <atul.baluni@shoplc.com>
2024-09-06 13:49:54 +0530

    code sync

--- src/components/Player/PlayerControls.js
+++ src/components/Player/PlayerControls.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect, useLayoutEffect } from "react";
 import HorizontalList from "../../helper/HorizontalList";
-
+ 
 import { VideoContext } from "../../utility/context";
 import { globals } from "../../global";
 import ToggleItem from "../ToogleItem";
@@ -10,18 +10,16 @@ import VerticalList from "../../helper/VerticalList";
 import { mapChannel } from "../../helper/mapper/mapChannelEpg";
 import downArrow from "../../assets/images/downArrow.png";
 import ProgramDetail from "../programDetail.js/ProgramDetail";
-const PlayerControls = ({ selectedAsset, setSelectedAsset   }) => {
+const PlayerControls = ({ selectedAsset, setSelectedAsset }) => {
   const { isActive, setIsActive } = useContext(VideoContext);
   const { fullscreen, setFullscreen } = useContext(VideoContext);
-
+ 
   const { currentTime } = useContext(VideoContext);
-  const { bufferedEnd} = useContext(VideoContext);
+  const { bufferedEnd } = useContext(VideoContext);
   const [previousChannel, setPreviousChannel] = useState(null);
   const [nextChannel, setNextChannel] = useState(null);
   const [value, setValue] = useState(0);
-
-
-  
+ 
   const handleSetActive = (status) => {
     setIsActive(status);
   };
@@ -47,35 +45,29 @@ const PlayerControls = ({ selectedAsset, setSelectedAsset   }) => {
       }
     }
   }, [selectedAsset]);
-
+ 
   useEffect(() => {
     const percentage = (currentTime / bufferedEnd) * 100;
     setValue(percentage);
-}, [currentTime, bufferedEnd]);
-const handleSeek = (time) => {
-    const player = document.querySelector('video');
+  }, [currentTime, bufferedEnd]);
+  const handleSeek = (time) => {
+    const player = document.querySelector("video");
     player.currentTime = time;
-};
-const handleSeekChange = (e) => {
+  };
+  const handleSeekChange = (e) => {
     const newTime = (e.target.value / 100) * bufferedEnd;
     setValue(e.target.value);
-  handleSeek(newTime);
-};
-
-const handlefullscreen =()=>{
-  
-  if(fullscreen == false){
-    setFullscreen(true)
-
-  }else{
-    setFullscreen(false)
-  }
-
-
-
-}
-
-
+    handleSeek(newTime);
+  };
+ 
+  const handlefullscreen = () => {
+    if (fullscreen == false) {
+      setFullscreen(true);
+    } else {
+      setFullscreen(false);
+    }
+  };
+ 
   return (
     <>
       <VerticalList
@@ -86,13 +78,13 @@ const handlefullscreen =()=>{
         className="w-full justify-center gap-3 items-center text-2xl flex"
         retainLastFocus={true}
         id={globals.COMPONENT_NAME.Player_Detail}
-        style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
-      >
-        <ProgramDetail />
+        style={{ opacity: isActive  ? 0 : fullscreen ? 0:1 , zIndex: isActive ? -1 : fullscreen ? -1:1 }}
+        >
+        <ProgramDetail onBack={() => handlefullscreen()} />
       </VerticalList>
       <div
-        style={{ opacity: isActive ? 0 : 1, zIndex: isActive ? -1 : 1 }}
-        className="flex justify-center absolute bottom-2 right-40  w-[80%] h-[16%] m-auto z-80"
+      style={{ opacity: isActive  ? 0 : fullscreen ? 0:1, zIndex: isActive ? -1 : fullscreen ? -1:1}}
+      className="flex justify-center absolute bottom-2 right-40  w-[80%] h-[16%] m-auto z-80"
       >
         <VerticalList>
           <HorizontalList
@@ -105,70 +97,15 @@ const handlefullscreen =()=>{
             id={globals.COMPONENT_NAME.Player_Control}
           >
             <div id="seekbar">
-              <ToggleItem className="">
+              <ToggleItem className="" onBack={() => handlefullscreen()}  parentNav="seekbar" >
                 {/* <input  type="range"  min="0"  max="100"  value={value} onChange={handleSeekChange} className="seek-bar"/> */}
                 <div className="seek-bar">
-                  <div className="filled" style={{ width: value+"%" }}></div>
+                  <div className="filled" style={{ width: value + "%" }}></div>
                 </div>
               </ToggleItem>
             </div>
           </HorizontalList>
-
-        <HorizontalList
-          onFocus={(index) => onFocus(index, globals.COMPONENT_NAME.Player_Control)}
-          onBlur={(index) => handleSetActive(true, index)}
-          className="w-full justify-center gap-3 items-center text-2xl flex"
-          retainLastFocus={true}
-          id={globals.COMPONENT_NAME.Player_Control}
-        >
-          <div className="player-next-prev">
-            <ToggleItem
-              allowedDirection={"right"}
-              parentId={globals.COMPONENT_NAME.Player_Control}
-              disabled={previousChannel ? false : true}
-              className={
-                previousChannel
-                  ? "absolute bottom-10 left-[125px]"
-                  : "disabled-button"
-              }
-              onEnter={() => setSelectedAsset(previousChannel)}
-              onBack={()=>handlefullscreen()}
-            >
-              <div className="channel-detail">pre</div>
-              <div className="channel-image">
-
-              <img src={previousChannel?previousChannel.baseSourceLocation+previousChannel.image.logo.tv:""}  alt="runnTV_downArrow"></img>
-              </div>
-            </ToggleItem>
-                   <>
-                   <img id="downArrow" className="absolute bottom-10" src={downArrow} alt="runnTV_downArrow"></img>
-                   </>
-            
-            <ToggleItem
-              allowedDirection={"left"}
-              parentId={globals.COMPONENT_NAME.Player_Control}
-              disabled={nextChannel ? false : true}
-              className={
-                nextChannel
-                  ? "absolute bottom-10 right-[110px]"
-                  : "disabled-button"
-              }
-              onEnter={() => setSelectedAsset(nextChannel)}
-              onBack={()=>handlefullscreen()}
-            >
-             
-              <div className="channel-image">
-
-              <img src={nextChannel?nextChannel.baseSourceLocation+nextChannel.image.logo.tv:""}  alt="runnTV_downArrow"></img>
-              </div>
-              <div className="channel-detail">next</div>
-            </ToggleItem>
-          </div>
-        </HorizontalList>
-
-        
-      </VerticalList>
-    </div>
+ 
           <HorizontalList
             onFocus={(index) =>
               onFocus(index, globals.COMPONENT_NAME.Player_Control)
@@ -189,6 +126,7 @@ const handlefullscreen =()=>{
                     : "disabled-button"
                 }
                 onEnter={() => setSelectedAsset(previousChannel)}
+                onBack={() => handlefullscreen()}
               >
                 <div className="channel-detail">pre</div>
                 <div className="channel-image">
@@ -211,7 +149,7 @@ const handlefullscreen =()=>{
                   alt="runnTV_downArrow"
                 ></img>
               </>
-
+ 
               <ToggleItem
                 allowedDirection={"left"}
                 parentId={globals.COMPONENT_NAME.Player_Control}
@@ -222,6 +160,7 @@ const handlefullscreen =()=>{
                     : "disabled-button"
                 }
                 onEnter={() => setSelectedAsset(nextChannel)}
+                onBack={() => handlefullscreen()}
               >
                 <div className="channel-image">
                   <img
@@ -243,5 +182,5 @@ const handlefullscreen =()=>{
     </>
   );
 };
-
+ 
 export default PlayerControls;
